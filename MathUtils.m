be6d7ebfd46cc28cb852dfc93e87ef6677f7e918
Bryan Lai <bryanlais@gmail.com>
2019-06-07 13:55:09 +0800

    Update README & dependencies

--- MathUtils.m
+++ MathUtils.m
@@ -1,55 +1,109 @@
+(* ::Package:: *)
+
 BeginPackage["Utils`"]
+packageName = Context[];
 
 Utils::usage =
     "Useful utilities for generic notebooks."
 
 
 "### Context Management";
-prependContext[context_] := If[ ! MemberQ[$ContextPath, #],
+Funcs`prependContext[context_] := If[ ! MemberQ[$ContextPath, context],
     PrependTo[$ContextPath, context];
 ];
+"# Access Funcs directly, within package";
+Funcs`prependContext["Funcs`"];
 
 
 "### Auto Collapse Cell";
-autoCollapse[] :=(
+Funcs`autoCollapse[] :=(
     If[ $FrontEnd =!= $Failed,
         SelectionMove[EvaluationNotebook[], All, GeneratedCell];
         FrontEndTokenExecute["SelectionCloseUnselectedCells"]
     ]
 );
 
-SetAttributes[hideText, HoldFirst];
-hideShow[x_, styles_: {}] := (
+SetAttributes[Funcs`hideShow, HoldFirst];
+Funcs`hideShow[x_, styles_: {}] := (
     Print[
         Style[x, Sequence @@ styles]
     ];
-    autoCollapse[]
+    autoCollapse[];
 );
 
-SetAttributes[hideInfo, HoldFirst];
-hideInfo[info_, styles_: {}] := hideShow[
+SetAttributes[Funcs`hideInfo, HoldFirst];
+Funcs`hideInfo[info_, styles_: {}] := hideShow[
     "### " <> ToString[info],
     {Bold}~Join~styles
 ];
 
 
-"### Hold Items for Further processing";
-SetAttributes[holdItems, HoldAll]
-holdItems[list_] := ReleaseHold[
+"### Hold Items for Further Processing";
+SetAttributes[Funcs`holdItems, HoldAll]
+Funcs`holdItems[list_] := ReleaseHold[
     MapAt[HoldForm, Hold[list], {All, All}]
 ];
 
 
 "### Plot Utils";
-colorPalette[theme_] := (
+Funcs`colorPalette[theme_] := (
     ("DefaultPlotStyle" /. (
         Method /. Charting`ResolvePlotTheme[theme, ListPlot]
     )) /. Directive[x_, __] :> x
 );
 
-getPlotPoints[plot_] := Cases[
-    plot, Line[pts_] :> pts, Infinity
+Funcs`getPlotPoints[plot_] := Cases[
+    plot, Line[pts_] :> Line[pts], Infinity
+] // Flatten // Cases[
+    #, Line[pts_] :> Sequence @@ pts, Infinity
+] &;
+
+"### Export Plots";
+SetAttributes[Funcs`exportPlot, HoldAll];
+Funcs`exportPlot[plot_, dir_: "plots/"] := Export[
+    dir <> ToString[HoldForm[plot]] <> ".pdf",
+    ReleaseHold[plot]
 ];
 
 
+"### Import Notebook";
+Funcs`importNotebook[path_, open_: False] := Module[{
+        nb, nbPath, context, action
+    },
+    context = Context[];
+    nbPath = FindFile[path];
+    nb = NotebookOpen[
+        nbPath,
+        CellContext -> context,
+        Visible -> None
+    ];
+    NotebookEvaluate[nb];
+    NotebookClose[nb];
+
+    action := NotebookOpen[nbPath];
+    If[open, action];
+    Button[path, action]
+];
+
+"### Generate GIF From Manipulate[]";
+Funcs`manipulateGif[manipulate_, name_String, step_Integer] := Export[
+    name <> ".gif",
+    Import[
+        Export[name <> ".avi", manipulate]
+        , "ImageList"
+    ][[1 ;; -1 ;; step]]
+];
+
+"### Show Exec Time & Pass Output";
+SetAttributes[Funcs`timeExec, {HoldAll, SequenceHold}];
+Funcs`timeExec[operations__] := (
+   Print[Now];
+   (Print[#1]; #2) & @@ Timing[operations]
+);
+
+
 EndPackage[]
+
+
+"### Access Funcs directly, in parent namespace";
+Funcs`prependContext["Funcs`"];
--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # PKUModernPhyLabReport_LaTeX
-LaTeX lab report for modern physics experiments @ PKU.
-
-北京大学物理学院 近代物理实验报告 - 2017~2018学年
+LaTeX lab report for modern physics experiments @ PKU. <br/>
+北京大学物理学院 近代物理实验报告 - 2017秋 + 2019春
 
 ## PDF
 > [1.1. 电子衍射 / ediff.pdf](1.1.%20电子衍射/ediff.pdf)<br/>
@@ -11,6 +10,15 @@ LaTeX lab report for modern physics experiments @ PKU.
 > [1.5. 霍尔系数 / silicon.pdf](1.5.%20霍尔系数/silicon.pdf)<br/>
 > [1.6. 扫描电镜 / sem.pdf](1.6.%20扫描电镜/sem.pdf)<br/>
 > [1.7. 康普顿散射 / compton.pdf](1.7.%20康普顿散射/compton.pdf)<br/>
+> ---
+> [2.1. 核磁共振 / nmr.pdf](2.1.%20核磁共振/nmr.pdf)<br/>
+> [2.2. 穆斯堡尔效应 / mossbauer.pdf](2.2.%20穆斯堡尔效应/mossbauer.pdf)<br/>
+> [2.3. 核磁成像 / mri.pdf](2.3.%20核磁成像/mri.pdf)<br/>
+> [2.4. 闪烁谱仪 / spectrum.pdf](2.4.%20闪烁谱仪/spectrum.pdf)<br/>
+> [2.5. 对流斑图 / convection.pdf](2.5.%20对流斑图/convection.pdf)<br/>
+> [2.6. 磁光克尔 / moke.pdf](2.6.%20磁光克尔/moke.pdf)<br/>
+> ---
+> 报告：[利用 Notebook 计算并记录能谱](2.4.%20闪烁谱仪/beamer/experiment_talk.pdf)
 
 * 提供近代实验报告供大家参考 ~
 * 遵循 [CC BY-NC-SA-4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/) 许可协议
@@ -18,5 +26,10 @@ LaTeX lab report for modern physics experiments @ PKU.
 * 导言区（preamble）略微混乱，有许多曾经用过没有删掉的宏包，欢迎精简
 
 P.S. 项目参考了 [chaserhkj/ModPhyLab](https://github.com/chaserhkj/ModPhyLab), 特此表示感谢～
+
+## 依赖关系
+* 所有报告共用根目录下的 `BibStyle/` 和 `PKUPhy.png`
+* 第二部分（编号 `2.*.`）的报告共用 `preamble.tex` 和 `MathUtils.m`
+* 有的 Mathematica 文档中引用了别处的 `MathUtils.m` 或未附上的 `*.bmp` 源文件，可能需要修正后方可正常运行
 
 Bryan
